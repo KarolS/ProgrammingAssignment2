f40bead0647c1724f9f4588f853424c48e42f800
KarolS <karol.m.stasiak@gmail.com>
2014-04-26 02:30:55 +0200

    Solved PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions providing wrappers for matrices that cache inverses of the matrices.
 
-## Write a short comment describing this function
+
+## Creates a wrapper for a matrix, which is a list containing following 4 functions:
+## get - to get the wrapped matrix
+## set - to set the wrapped matrix (clearing the inverse)
+## getinverse - to get the cached inverse of the wrapped matrix (or NULL if not yet calculated)
+## setinverse - to set the inverse of the wrapped matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates an inverse of given wrapped matrix. 
+## If the wrapper contains a cached inverse, it is returned withour recalculating.
+## If it doesn't, the calculated inverse is stored in the wrapper.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        ## message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
